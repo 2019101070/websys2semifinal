a9a045b1d4f277d2882b9b0ce4c2b946c9940fa2
Tim <forgamingaccsonly@gmail.com>
2022-05-13 17:57:43 +0800

    git commit -m edit

--- pages/users.php
+++ pages/users.php
@@ -2,18 +2,34 @@
     <head>
         <title>List of Users</title>
     </head>
+    <style type="text/css">
+        td
+        {
+            border: 1px solid;
+        }
+        </style>
         <body>
             <?Php
             require_once('../dao/config.php');
             require_once('../dao/crudDAO.php');
             ?>
             <table>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Specific Time</th>
+                <th>Class Details</th>
+                <th>Gender</th>
                 <?Php $a=new crudDAO();
                 $list = $a->display();
                 foreach($list as $key => $value){
                     echo '<tr>';
                     echo '<td>'.$value['name'].'</td>';
-                    
+                    echo '<td>'.$value['email'].'</td>';
+                    echo '<td>'.$value['specifictime'].'</td>';
+                    echo '<td>'.$value['classdetails'].'</td>';
+                    echo '<td>'.$value['gender'].'</td>';
+                    echo '<td><a href ="">Edit</a></td>';
+
                 }
                 ?>
         </body>
